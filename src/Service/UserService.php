d1484541bd543f6d3c9ed17a6bdfbc5ea485ec95
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2020-05-06 18:48:03 +0900

    user service

---
+++ src/Service/UserService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+
+class UserService
+{
+    public function __construct(private UserRepository $repo) {}
+
+    public function all(): array { return $this->repo->findAll(); }
+
+    public function get(int $id): ?User { return $this->repo->find($id); }
+
+    public function create(User $e): User { $this->repo->save($e); return $e; }
+}
