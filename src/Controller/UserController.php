a2288940a31fc32ceee12fa2270f80ffbed3738a
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2020-05-08 16:17:10 +0900

    user controller

---
+++ src/Controller/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\UserService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/users')]
+class UserController extends AbstractController
+{
+    public function __construct(private UserService $service) {}
+
+    #[Route('', methods: ['GET'])]
+    public function list(): JsonResponse { return $this->json($this->service->all()); }
+
+    #[Route('/{id}', methods: ['GET'])]
+    public function get(int $id): JsonResponse
+    {
+        $e = $this->service->get($id);
+        return $e ? $this->json($e) : $this->json(['error' => 'not found'], 404);
+    }
+}
