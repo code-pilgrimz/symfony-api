6ad8c3de2ccf478b78be835a63ee7aa49fbf296f
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2020-06-09 14:59:15 +0900

    task repository

---
+++ src/Repository/TaskRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Task;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Task>
+ */
+class TaskRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Task::class);
+    }
+
+    public function save(Task $e): void
+    {
+        $this->getEntityManager()->persist($e);
+        $this->getEntityManager()->flush();
+    }
+}
