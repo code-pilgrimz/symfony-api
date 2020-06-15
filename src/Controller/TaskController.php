1c3d078cfba45a142a9e8c237f95aae9f2073da1
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2020-06-15 22:50:52 +0900

    task controller

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\TaskService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/tasks')]
+class TaskController extends AbstractController
+{
+    public function __construct(private TaskService $service) {}
+
+    #[Route('', methods: ['GET'])]
+    public function list(): JsonResponse { return $this->json($this->service->all()); }
+
+    #[Route('/{id}', methods: ['GET'])]
+    public function get(int $id): JsonResponse
+    {
+        $e = $this->service->get($id);
+        return $e ? $this->json($e) : $this->json(['error' => 'not found'], 404);
+    }
+}
