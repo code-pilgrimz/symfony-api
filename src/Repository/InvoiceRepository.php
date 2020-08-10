4fbf1b3b955f2053f8a7727c2ffff4af63a0137f
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2020-08-10 14:11:34 +0900

    invoice repository

---
+++ src/Repository/InvoiceRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Invoice;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Invoice>
+ */
+class InvoiceRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Invoice::class);
+    }
+
+    public function save(Invoice $e): void
+    {
+        $this->getEntityManager()->persist($e);
+        $this->getEntityManager()->flush();
+    }
+}
