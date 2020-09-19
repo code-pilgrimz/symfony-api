71e866b4aa66f278d43f0b1c5277569182d5551d
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2020-09-19 11:12:46 +0900

    notification repository

---
+++ src/Repository/NotificationRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Notification;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Notification>
+ */
+class NotificationRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Notification::class);
+    }
+
+    public function save(Notification $e): void
+    {
+        $this->getEntityManager()->persist($e);
+        $this->getEntityManager()->flush();
+    }
+}
