122be9d5e1a5292d11aba6bd6e32efd9be23a4a4
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2020-10-13 10:13:38 +0900

    webhook repository

---
+++ src/Repository/WebhookRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Webhook;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Webhook>
+ */
+class WebhookRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Webhook::class);
+    }
+
+    public function save(Webhook $e): void
+    {
+        $this->getEntityManager()->persist($e);
+        $this->getEntityManager()->flush();
+    }
+}
