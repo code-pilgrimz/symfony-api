58dc4b1518b0d061b2f73b4759a28a1ffa35b40c
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2020-10-19 14:25:35 +0900

    webhook controller

---
+++ src/Controller/WebhookController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\WebhookService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/webhooks')]
+class WebhookController extends AbstractController
+{
+    public function __construct(private WebhookService $service) {}
+
+    #[Route('', methods: ['GET'])]
+    public function list(): JsonResponse { return $this->json($this->service->all()); }
+
+    #[Route('/{id}', methods: ['GET'])]
+    public function get(int $id): JsonResponse
+    {
+        $e = $this->service->get($id);
+        return $e ? $this->json($e) : $this->json(['error' => 'not found'], 404);
+    }
+}
